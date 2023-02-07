3df639226ed9ec6dd641f37da7c488d4a87f28a5
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-02-07 13:50:57 +0100

    warmups

--- src/mods/runner/global.ts
+++ src/mods/runner/global.ts
@@ -3,32 +3,51 @@ import { BenchError } from "mods/runner/error.js";
 import { Result } from "mods/runner/result.js";
 
 export interface BenchParams {
-  samples?: number
+  samples?: number,
+  warmup?: boolean
 }
 
 export async function bench(message: string, closure: (context: Context) => Promise<void>, params: BenchParams = {}) {
   try {
-    const { samples = 1_000_000 } = params
+    const { samples = 1_000_000, warmup = true } = params
 
     const context = new Context(message)
+
+    if (warmup) {
+      const warmups = new Array<number>(samples)
+
+      for (let i = 0; i < samples; i++) {
+        const start = performance.now()
+        await closure(context)
+        const end = performance.now()
+
+        const result = (end - start)
+        warmups[i] = result
+      }
+    }
+
     const results = new Array<number>(samples)
 
     let minimum = Number.MAX_SAFE_INTEGER
     let maximum = 0
 
+    let sum = 0
+
     for (let i = 0; i < samples; i++) {
       const start = performance.now()
       await closure(context)
       const end = performance.now()
+
       const result = (end - start)
+      results[i] = result
+
       if (result < minimum)
         minimum = result
       if (result > maximum)
         maximum = result
-      results[i] = result
+      sum += result
     }
 
-    const sum = results.reduce((p, c) => p + c, 0)
     const average = sum / samples
 
     return new Result(message, samples, average, minimum, maximum, results)
@@ -39,27 +58,45 @@ export async function bench(message: string, closure: (context: Context) => Prom
 
 export function benchSync(message: string, closure: (context: Context) => void, params: BenchParams = {}) {
   try {
-    const { samples = 1_000_000 } = params
+    const { samples = 1_000_000, warmup = true } = params
 
     const context = new Context(message)
+
+    if (warmup) {
+      const warmups = new Array<number>(samples)
+
+      for (let i = 0; i < samples; i++) {
+        const start = performance.now()
+        closure(context)
+        const end = performance.now()
+
+        const result = (end - start)
+        warmups[i] = result
+      }
+    }
+
     const results = new Array<number>(samples)
 
     let minimum = Number.MAX_SAFE_INTEGER
     let maximum = 0
 
+    let sum = 0
+
     for (let i = 0; i < samples; i++) {
       const start = performance.now()
       closure(context)
       const end = performance.now()
+
       const result = (end - start)
+      results[i] = result
+
       if (result < minimum)
         minimum = result
       if (result > maximum)
         maximum = result
-      results[i] = result
+      sum += result
     }
 
-    const sum = results.reduce((p, c) => p + c, 0)
     const average = sum / samples
 
     return new Result(message, samples, average, minimum, maximum, results)
